997505ce0e14699a5d9b29a8c26dcfd85e43e9d7
Oseguera, Joshua <joshoseguera09@gmail.com>
2023-07-12 20:04:48 -0700

    created function countdown timer. linked index and highscore pages.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,185 @@
 
 
-var correctEl = document.querySelector(".questions_correct");
-var incorrectEl = document.querySelector(".questions_incorrect");
-var timerEl = document.querySelector(".timer");
-var startButtonEl = document.querySelector(".start-button");
 var quizBoardEl = document.querySelector(".quiz-board");
-var quizResultEl = document.querySelector(".quiz-board_result");
-var quizDisplayEl = document.querySelector(".quiz-board_display");
-var controlsEl = document.querySelector(".controls");
+var quizTimerEl = document.querySelector(".quiz-timer");
+var startButtonEl = document.querySelector("#start-button");
+var answerButtonEl = document.querySelector(".button-answer");
+var scoreEl = document.querySelector(".score");
+var quizControlsEl = document.querySelector(".quiz-controls");
 
+var duration = 3;
+var score = 0;
+var currentQuestionIndex;  
 
-var correct = 0;
-var incorrect = 0;  
-var highScore = 0;
-var timer = null;
-var timeLeft = 0;
-var currentQuestionIndex; 
-
-
-const questionBank = [
+var questionBank = [
     "Which of the following technologies is primarily used to structure a webpage? HTML. CSS. JavaScript. Node.js.",
+    
     "Which of the following technologies is primarily used to style a webpage? Chrome. CSS. JavaScript. SQL.",
+    
     "Which of the following technologies is primarily used to add user interactivity to a webpage? Chrome. HTML. JavaScript. CSS.",
+    
     "array[1] will select which element in an array? The 1st. The 2nd. The 3rd. None.",
+    
     ".light correlates with which of the following CSS selectors? class. ID. pseudo-class. element. ",
 ];
 
-const duration = 20;
 
+// // Event: Page Load 
 
-// Event: Page Load 
+//     // retrieve data from persistance
 
+//     var scores = JSON.parse(localStorage.getItem("storageKey"));
 
+//     // Update State
+
+//     if (scores) {
+//         correct = scores.correct;
+//         losses = scores.losses;
+//     }
+
+//     // Update the UI
+//     function updateScoreboard(){}
 
 
 // Event: Click Start
 
-function startQuiz(event){
+function startQuiz(){
     console.log("Quiz Started.")
-};
+
+    countDown = setInterval(timerCountDown, 1000);
+
+    function timerCountDown(){
+        
+        startButtonEl.className += "hide";
+        answerButtonEl.classList.remove("hide")
+        
+        duration--;
+        quizTimerEl.textContent = duration
+        if(duration === 0) {
+            clearInterval(countDown);
+            
+            console.log("Quiz has ended.");
+
+
+            
+            
+
+
+
+        }    
+    }
+}
 
 startButtonEl.addEventListener("click", startQuiz);
 
 
-// Event: Timer Countdown
 
-function timerCountDown(event){
-    console.log("time is ticking.");
-}
-
-
-// Event: Click Answer Button
-
-function clickAnswer(event){
-    console.log("button was clicked.");
-}
-
-// startButtonEl.addEventListener("click", startQuiz);   ========== will become answer button.
+    //     currentQuestionIndex = Math.floor(Math.random()*questionBank.length);
+        
+    //     // set the correct answer ... likely not needed.
+    //     currentGuess = new Array(questionBank[currentQuestionIndex].length).fill("_")
 
 
+    //     // hide the start button
+    //     hideElement(controlsEl);
 
-// Event: Game Ends
+    //     // reset the board
 
-function quizOver(event){
-    console.log("quiz is now over.")
-}
+    //     // hide any result message
+    //     hideElement(quizResultEl);
+
+    //     // show the timer 
+    //     showElement(timerEl);
+        
+    //     // show the quiz  board
+    //     showElement(gamebaordEl);
+
+    //     // show the word display
+    //     updateWordDisplay();
+
+    // }
+
+
+
+// // Event: Timer Countdown
+
+
+
+
+// // Event: Click Answer Button
+
+// function clickAnswer(event){
+//     console.log("button was clicked.");
+// }
+
+// // startButtonEl.addEventListener("click", startQuiz);   ========== will become answer button.
+
+
+
+// // Event: Game Ends
+
+// function quizOver(didWin){
+//     clearInterval(timer);
+//     timer = null;
+
+//     // Update state
+//     if(didWin){
+//         wins++;
+//     } else {
+//         losses++;
+//     }
+
+// localStorage.setItem(storageKey, JSON.stringify({wins: wins, losses: losses}))
+
+//     // Update UI
+
+
+
+//     // Display Result Message
+// displayResult(didWin);
+// updateScoreboard();
+// showElement(controlsEl);
+
+
+
+// }
+
+
+
+// // Additional functions
+
+// function updateScoreboard(){
+    
+// }
+
+
+
+
+// function hideElement(element) {
+//     element.classList.add("hide");
+// }
+
+// function showElement(element) {
+//     element.classList.remove("hide");
+
+// function displayResult(didWin) {
+    
+//     gameresultEl.classList.remove("Success");
+//     gameresultEl.classList.remove("fail");
+
+    
+//     hideElement(timerEl);
+
+//     if(didWin) {
+//         gameresultEl.textContent("You Win!");
+//         gameresultEl.classList.add("success");
+//     } else {
+//         gameresultEl.textContent("You Lost!");
+//         gameresultEl.classList.add("failure");
+//     }
+// }
+//     showElement(gameresultEl);
+// }
+
+// function updateWordDisplay(){
+    // quizDisplayEl.textContent = currentGuess.join(" ");
