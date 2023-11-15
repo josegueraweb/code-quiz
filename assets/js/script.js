4e7dbd42a7ae840fb80beebffb2994d6a14b016d
Oseguera, Joshua <joshoseguera09@gmail.com>
2023-11-14 18:47:39 -0800

    Established the HTML structure for when the application is in its 'before the quiz begins' state. Created and loosely established the HTML structure of the 'High Scores' page and linked it to the application homepage. Created and tested an interval timer that will count down from 30 when the quiz 'start button' is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,26 @@
-var quizBoardEl = document.querySelector(".quiz-board");
-var quizTimerEl = document.querySelector(".quiz-timer");
-var startButtonEl = document.querySelector("#start-button");
-var answerButtonEl = document.querySelector(".button-answer");
-var scoreEl = document.querySelector(".score");
-var quizControlsEl = document.querySelector(".quiz-controls");
+// hooks into the DOM
+var timer = document.querySelector("#timer");
+var startbtn = document.querySelector("#start-btn");
+var quizEndMsg = document.querySelector(".quiz-end-msg");
 
-var duration = 20;
-var scoreEl = 0;
-var currentQuestionIndex;
+// state variable to keep track of time. 
+var secondsLeft = 10;
 
-var questionBank = [
-    "Which of the following technologies is primarily used to structure a webpage? HTML. CSS. JavaScript. Node.js.",
-
-    "Which of the following technologies is primarily used to style a webpage? Chrome. CSS. JavaScript. SQL.",
-
-    "Which of the following technologies is primarily used to add user interactivity to a webpage? Chrome. HTML. JavaScript. CSS.",
-
-    "array[1] will select which element in an array? The 1st. The 2nd. The 3rd. None.",
-
-    ".light correlates with which of the following CSS selectors? class. ID. pseudo-class. element. ",
-];
-
-// // Event: Page Load 
-
-function init() {
-    console.log("Quiz loading...");
-}
-
-    // Get data from persistence
-    // var scores = JSON.parse(localStorage.getItem(storageKey));
-
-// Event: Click Start
-
-function startQuiz() {
-    console.log("Quiz Started.")
-        countDown = setInterval(timerCountDown, 1000);
-
-        function timerCountDown() {
-            console.log("tick");
-            duration--;
-            quizTimerEl.textContent = "Time Left: " + duration
-            if (duration === 0) {
-                quizOver(false);
-                duration = null;
-                var quizOverText = document.createElement("h1");
-                quizOverText.textContent = "QUIZ OVER!"
-                document.body.appendChild(quizOverText);
-            }
+// function that will start quiz game
+function startGame() {
+    var countdownTimer = setInterval(function () {
+        quizEndMsg.textContent = " ";
+        timer.textContent = "Time Remaining: " + secondsLeft;
+        console.log("Time remaining: ", secondsLeft);
+        secondsLeft--;
+        if (secondsLeft === -2) {
+            clearInterval(countdownTimer);
+            timer.textContent = " ";
+            quizEndMsg.textContent = "Time's Up!";
+            secondsLeft = 10;
         }
-    
-        timerCountDown();
+    }, 1000);
+};
 
-        hideElement(startButtonEl);
-        hideElement(quizResultEl);
-        showElement(duration);
-        showElement(quizBoardEl);
-    }
-
-// Event: Click Answer Button
-
-function clickAnswer(event){
-    console.log("answer button clicked");
-}
-
-startButtonEl.addEventListener("click", startQuiz);
-
-answerButtonEl.addEventListener("click", clickAnswer);
-
-// Event: Game Ends
-
-    function quizOver(Win) {
-        clearInterval(countDown);
-        duration = null;
-        hideElement(quizTimerEl);
-        console.log("Quiz has ended.");
-
-    }
-
-    function hideElement(element) {
-        element.classList.add("hide");
-    }
-
-    function showElement(element) {
-        element.classList.remove("hide");
-    }
-
-init();
+// "click" event listener that will execute the "startGame" function when the start button is pressed. 
+startbtn.addEventListener("click", startGame);
---
+++ assets/question-bank.md
@@ -0,0 +1,53 @@
+1. What is HTML?
+a) HTML describes the structure of a webpage
+b) HTML is the standard markup language mainly used to create web pages
+c) HTML consists of a set of elements that helps the browser how to view the content
+d) All of the mentioned
+
+2. Which of the following is used to read an HTML page and render it?
+a) Web server
+b) Web network
+c) Web browser
+d) Web matrix
+
+3. In which part of the HTML metadata is contained?
+a) head tag
+b) title tag
+c) html tag
+d) body tag
+
+4. What is CSS?
+a) CSS is a style sheet language
+b) CSS is designed to separate the presentation and content, including layout, colors, and fonts
+c) CSS is the language used to style the HTML documents
+d) All of the mentioned
+
+5. Which of the following CSS selectors are used to specify a group of elements?
+a) tag
+b) id
+c) class
+d) both class and tag
+
+6. Which of the following is the correct way to apply CSS Styles?
+a) in an external CSS file
+b) inside an HTML element
+c) inside the <head> section of an HTML page
+d) all of the mentioned
+
+7. What is JavaScript?
+a) JavaScript is a scripting language used to make the website interactive
+b) JavaScript is an assembly language used to make the website interactive
+c) JavaScript is a compiled language used to make the website interactive
+d) None of the mentioned
+
+8. Which of the following is not javascript data types?
+a) Null type
+b) Undefined type
+c) Number type
+d) All of the mentioned
+
+9. Which of the following can be used to call a JavaScript Code Snippet?
+a) Function/Method
+b) Preprocessor
+c) Triggering Event
+d) RMI
